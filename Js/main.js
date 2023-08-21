8be38db74f53d60e66ed72c7224f966b5f199e92
Catiflam <gilfrid1@gmail.com>
2023-08-21 15:44:50 +0200

    edit fix grid

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,10 @@
-const cellTotal = 5 * 5;
+const cellTotal = 10 * 10;
 const cellContainer = document.getElementById("cell-container");
 const generateButton = document.getElementById("generate-grid");
+
+for (let i = 1; i <= cellTotal; i++) {
+	const cell = document.createElement("li");
+	cell.innerText = 1;
+	cell.classList.add("cell");
+	cellContainer.append(cell);
+}
