3432f269ba1e0172433296ae3e71be8906762247
Catiflam <gilfrid1@gmail.com>
2023-08-22 09:21:33 +0200

    last commit

--- Js/main.js
+++ Js/main.js
@@ -2,9 +2,26 @@ const cellTotal = 10 * 10;
 const cellContainer = document.getElementById("cell-container");
 const generateButton = document.getElementById("generate-grid");
 
-for (let i = 1; i <= cellTotal; i++) {
+generateButton.addEventListener("click", function () {
+	generateGrid(cellTotal, cellContainer);
+});
+
+function createGrid(container, i) {
 	const cell = document.createElement("li");
-	cell.innerText = 1;
+	cell.setAttribute("data-index", i);
 	cell.classList.add("cell");
-	cellContainer.append(cell);
+	container.append(cell);
+	cell.addEventListener("click", function () {
+		const index = this.getAttribute("data-index");
+		this.innerText = index;
+		this.classList.add("cell-even");
+		console.log(this);
+	});
+}
+
+function generateGrid(celsNumber, container) {
+	container.innerHTML = "";
+	for (let i = 1; i <= celsNumber; i++) {
+		createGrid(container, i);
+	}
 }
